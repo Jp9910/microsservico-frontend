3b36bcc21c73a66ab32bcca711602934b892184a
João Secundo <jops40@hotmail.com>
2025-01-31 08:08:37 -0300

    Setup typescript+vite+react

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+function Botao (props: {legenda: string}) {
+    return (
+        <button>
+            {props.legenda}
+            {/* javascript pode ser escrito entre {} */}
+        </button>
+        // javascript pode ser escrito com //
+    )
+}
+
+export default Botao
